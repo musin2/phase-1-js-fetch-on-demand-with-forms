261adbf6e0118bcc0c2c50c078333eb6d7439ecb
musin2 <muhsindaud75@gmail.com>
2024-07-21 14:54:21 +0300

    adds lab code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
+    let form = document.querySelector('form')
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let input = document.querySelector('input#searchByID')
   
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(res => res.json())
+    .then(data => {
+      let title = document.querySelector('section#movieDetails h4')
+      let summary = document.querySelector('section#movieDetails p')
+  
+      title.innerText = data.title
+      summary.innerText = data.summary
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
